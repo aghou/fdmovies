65b2666d3b257ea049df6ebe2c45b5c0533de7ee
Zahir SAAD BOUZID <zahirnet@gmail.com>
2023-02-24 22:48:00 +0100

    feat(plugins): add mongodb connection service

---
+++ src/plugins/mongodb.ts
@@ -0,0 +1,33 @@
+import fp from 'fastify-plugin';
+import fastifyMongo from '@fastify/mongodb'
+
+/**
+ * Add support for Mongodb
+ */
+export const MongoDB =  fp(async (fastify) => {
+  try {
+    fastify.register(fastifyMongo, {
+      url: getMongodbURL(),
+      database: process.env.MONGO_DB || 'fdmovies'
+    })
+  } catch (e) {
+    // console.log(e)
+  }
+})
+
+/**
+ * Generate mongodb URL
+ * @return {string}
+ */
+export function getMongodbURL(): string {
+  const {MONGO_URL, MONGO_DB, MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD} = process.env;
+
+  if (MONGO_URL) {
+    return MONGO_URL;
+  }
+  let url = `mongodb://`;
+  url += (MONGO_USERNAME && MONGO_PASSWORD) ? `${MONGO_USERNAME}:${MONGO_PASSWORD}@` : '';
+  url += `${MONGO_HOST || 'localhost'}:${MONGO_PORT || '27017'}/${MONGO_DB || 'fdmovies'}`;
+  return url;
+}
+
